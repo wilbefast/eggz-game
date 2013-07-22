43dca686a7cfc4295fb28ed922581857cb8c1558
wilbefast <wilbefast@gmail.com>
2013-07-22 21:49:06 +0200

    clean workaround for love's faulty isKeyDown method

--- src/unrequited/input.lua
+++ src/unrequited/input.lua
@@ -16,18 +16,18 @@ local input = {}
 for i = 1, MAX_PLAYERS do
   input[i] = { x = 0, y = 0, lay = false, lay_prev = false, lay_trigger = 0}
 end
-input[1].keyleft = "left"
-input[1].keyright = "right"
-input[1].keyup = "up"
-input[1].keydown = "down"
- input[1].keylay = "rctrl"
---input[1].keylay = " "
+input[1].keyleft = function () return love.keyboard.isDown("left") end
+input[1].keyright = function () return love.keyboard.isDown("right") end
+input[1].keyup = function () return love.keyboard.isDown("up") end
+input[1].keydown = function () return love.keyboard.isDown("down") end
+input[1].keylay = function () return love.keyboard.isDown("rctrl") end
 
-input[2].keyleft = "a"
-input[2].keyright = "d"
-input[2].keyup = "w"
-input[2].keydown = "s"
-input[2].keylay = "lctrl"
+
+input[2].keyleft = function () return love.keyboard.isDown("a", "q") end
+input[2].keyright = function () return love.keyboard.isDown("d") end
+input[2].keyup = function () return love.keyboard.isDown("w", "z") end
+input[2].keydown = function () return love.keyboard.isDown("s") end
+input[2].keylay = function () return love.keyboard.isDown("lctrl") end
 
 function input:update(dt)
 
@@ -36,25 +36,25 @@ function input:update(dt)
     p.x, p.y = 0, 0
 
     --left
-    if p.keyleft and love.keyboard.isDown(p.keyleft) then
+    if p.keyleft and p.keyleft() then
       p.x = p.x - 1 
     end
     --right
-    if p.keyright and love.keyboard.isDown(p.keyright) then
+    if p.keyright and p.keyright() then
       p.x = p.x + 1 
     end
     -- up
-    if p.keyup and love.keyboard.isDown(p.keyup) then
+    if p.keyup and p.keyup() then
       p.y = p.y - 1 
     end
     -- down
-    if p.keydown and love.keyboard.isDown(p.keydown) then 
+    if p.keydown and p.keydown() then 
       p.y = p.y + 1 
     end
 
     -- lay
     if p.keylay then
-      p.lay = love.keyboard.isDown(p.keylay)
+      p.lay = p.keylay()
       if p.lay then
         if not p.lay_prev then
           p.lay_trigger = 1
